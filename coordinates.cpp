1850320bcd78d973f028c339f159ba5cbcb40879
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-08-17 21:24:05 +0200

    libhash++ is now shipped with coordinates.so

--- coordinates.cpp
+++ coordinates.cpp
@@ -1,11 +1,22 @@
 #include "coordinates.h"
 #include "hashlib/hashlibpp.h"
+#include <algorithm>
+#include <sstream>
 
+const char*
+uint2cstr( uint64_t i ) {
+  std::stringstream ss;
+  std::string temp;
+  ss << i;
+  ss >> temp;
+  return temp.c_str();
+}
 
 Ccoordinates::Ccoordinates() {
-	hashwrapper *myWrapper = new sha1wrapper();
-	position = "void";
-	parent_positions[0] = "void";
+	hashwrapper *sha1 = new sha1wrapper();
+	srand( (unsigned)time( NULL ) );
+	position = sha1->getHashFromString( (std::string)( uint2cstr( rand() % time( NULL ) ) ) );
+	parent_positions[0] = position;
 	parent_positions[1] = "void";
 	parent_positions[2] = "void";
 	parent_positions[3] = "void";
