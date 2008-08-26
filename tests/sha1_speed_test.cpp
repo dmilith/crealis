f9d4afc4381d6a83c19ce4060fed849caa40ae25
vara <vara@Liszaj.liszaj>
2008-08-26 18:44:04 +0200

    tescik

--- tests/sha1_speed_test.cpp
+++ tests/sha1_speed_test.cpp
@@ -1,24 +1,78 @@
 #include "../hashlib/hl_tools.h"
 #include <boost/thread/thread.hpp>
+#include <iostream>
+#include <ncurses.h>
+
+using namespace std;
+
+WINDOW *vin;
+
+
+char* itoa( int value, char* result, int base ) {
+	
+	if (base < 2 || base > 16) { 
+		*result = 0; return result; 
+	}
+	
+	char* out = result;	
+	int quotient = value;
+	
+	do {	
+		*out = "0123456789abcdef"[ abs( quotient % base ) ];
+	
+		++out;
+	
+		quotient /= base;
+	
+	} while ( quotient );
+	
+	if ( value < 0 && base == 10) *out++ = '-';	
+	reverse( result, out );	
+	*out = 0;	
+	return result;
+	
+}
 
 void
 thread_first() {
-  for ( int i = 0; i < 500000; i++ ) {
-    generate_sha1();
-  }
+	//cout<<"first_thread";
+	char result [100];
+  	for ( int i = 0; i < 1000; i++ ) {  	
+  		wmove(vin,2,2);
+    		wprintw(vin,itoa(i,result,10));
+        	wrefresh(vin);
+        	
+    		generate_sha1();
+  	}
 }
 
 void
 thread_second() {
-  for ( int i = 0; i < 500000; i++ ) {
-    generate_sha1();
-  }
+
+	//cout<<"second_thread";
+	char result [100];
+  	for ( int i = 0; i < 10000; i++ ) {
+  		wmove(vin,0,0);
+    		wprintw(vin,itoa(i,result,10));
+        	wrefresh(vin);
+    		generate_sha1();
+  	}
 }
 
+
 int main() {
-  boost::thread first_thread( &thread_first );
-  boost::thread second_thread( &thread_second );
-  first_thread.join();
-  second_thread.join();
-  return 0;
+	
+	initscr();
+        vin=newwin(40,40,0,0);
+        
+  	boost::thread first_thread( &thread_first );
+  	boost::thread second_thread( &thread_second );
+  	first_thread.join();
+  	//second_thread.join();
+  	
+        getchar();
+        delwin(vin);
+        endwin();
+  	
+  	return 0;
 }
