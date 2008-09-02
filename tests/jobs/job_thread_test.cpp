f4f38601cfdef8b361a918843de0a99e26e9bdc1
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-09-02 05:13:35 +0200

    additional modifications

--- tests/jobs/job_thread_test.cpp
+++ tests/jobs/job_thread_test.cpp
@@ -19,55 +19,57 @@
 using namespace std;
 using namespace core;
 
-
 extern void create_job( Cobject* object0, Cobject* object1 );
 
 
 #define DEBUG
 #define VERBOSE
 
+//Csoul souls[5000];
+
 
 void
 run() {
-	boost::xtime xt;
-  boost::mutex mutex;
-  Csoul *x = new Csoul();
-  Csoul *y = new Csoul();
-    x->set_mana( 1000 );
-    y->set_mana( 995 );
-    
-    assert( x->get_mana() == 1000 );
-    assert( y->get_mana() == 995 );
+      boost::mutex mutex;
 
-    Job *job = new Job();
-    job->create_job( *x, *y, action_ATTACK );
-    
-    assert( job->get_actor( 0 ).get_mana() == 1000 );
-    assert( job->get_actor( 1 ).get_mana() != 666 );
+      Csoul *x = new Csoul();
+      Csoul *y = new Csoul();
+      
+      x->set_mana( 1000 );
+      y->set_mana( 995 );
+      
+      assert( x->get_mana() == 1000 );
+      assert( y->get_mana() == 995 );
 
-    boost::mutex::scoped_lock lock( mutex );
-    job->run();
-    
-    assert( job->get_actor( 0 ).get_mana() < 1000 );
-    assert( job->get_actor( 1 ).get_mana() == 995 );
+      Job *job = new Job();
+      job->create_job( *x, *y, action_ATTACK );
+      
+      assert( job->get_actor( 0 ).get_mana() == 1000 );
+      assert( job->get_actor( 1 ).get_mana() != 666 );
 
-    boost::xtime_get( &xt, boost::TIME_UTC );
-    xt.nsec += 5000; // just slow down the loop
-    boost::thread::sleep( xt );
-  
-  delete x;
-  delete y;
-  delete job;
+      boost::mutex::scoped_lock lock( mutex );
+      job->run();
+      
+      assert( job->get_actor( 0 ).get_mana() < 1000 );
+      assert( job->get_actor( 1 ).get_mana() == 995 );
+    
+      delete x;
+      delete y;
+      delete job;
 }
 
 
 int
 main( int argc, char* argv[] ) {
   time_t start = time( NULL );
+  boost::xtime xt;
   boost::thread_group threads;
-   for ( int i = 0; i < 100000; ++i ) { /* on my machine 77824 is max, but 32760 is max after which boost_resource error
+   for ( int i = 0; i < 10000; ++i ) { /* on my machine 77824 is max, but 32760 is max after which boost_resource error
                                           is raised. ( cat /proc/sys/kernel/threads-max ) */
      threads.create_thread( &run );
+  //   boost::xtime_get( &xt, boost::TIME_UTC );
+  //   xt.nsec += 5000000; // just slow down the loop
+  //   boost::thread::sleep( xt );
 
 #ifdef DEBUG
      cout << "created thread " << i << endl << flush;
@@ -77,7 +79,7 @@ main( int argc, char* argv[] ) {
 #ifdef DEBUG
    cout << "Done. Quitting." << endl;
    cout << time( NULL ) - start << "seconds";
-   assert( time( NULL) - start <= 5 );
+   assert( time( NULL) - start <= 10 );
 #endif
    threads.join_all();
   return 0;
